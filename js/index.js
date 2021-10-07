767ed75e0b9142cc4cdb1d6b66a24a2339e55005
Ubuntu <ubuntu@ip-172-31-37-124.ap-northeast-1.compute.internal>
2021-10-07 02:49:35 +0000

    todo-list-show

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log('hello');
+//DOM
+const ul = document.getElementById("js-ul");
+const form = document.getElementById("js-form");
+const input = document.getElementById("js-input");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const value = input.value;
+    console.log(value);
+    add();
+});
+
+function add() {
+    let inputValue = input.value;
+    const li = document.createElement("li");
+    li.innerText = inputValue;
+    li.classList.add("top__container-list");
+    ul.appendChild(li);
+}
